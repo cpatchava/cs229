7ba77888f54deaea17d27567b6d2ffde466f15a2
Bavin Ondieki <bavin2009@gmail.com>
2013-11-24 18:01:50 -0800

    Edited —added a plotting tool to visualize the data. I did install matplotlib (a Python library with the functionality of matlab).
    I set it to plot the crime data each time the program runs.
    
    If you don’t have matplotlib installed, you can copy the code, and comment out the plotting function. I will add a flag so that plotting will be an option

--- path.py
+++ path.py
@@ -3,8 +3,58 @@ from googlemaps import GoogleMaps
 import csv
 import sys, copy
 import getopt
-import matplotlib
+import random
 
+#Plotting and Math Libraries/Tools
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import numpy as np
+from matplotlib import cm
+from matplotlib import pyplot as plt
+from matplotlib.mlab import griddata
+
+def plot2(X,Y,Z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+
+    ax.scatter(X,Y,Z, c ='r', marker = 'o')
+
+    ax.set_xlabel('x axis')
+    ax.set_ylabel('y axis')
+    ax.set_zlabel('z axis')
+    
+    plt.show()
+
+################################################
+#FUNCTION: plot
+        #:Plots the Crime Data on a meshgrid
+################################################
+def plot(x,y,z):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    xi = np.linspace(min(x), max(x))
+    yi = np.linspace(min(y), max(y))
+
+    X, Y = np.meshgrid(xi, yi)
+    Z = griddata(x, y, z, xi, yi)
+
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
+                           linewidth=0,antialiased=True)
+
+    ax.set_zlim3d(np.min(Z), np.max(Z))
+    fig.colorbar(surf)
+
+    ax.set_xlabel('Latitude')
+    ax.set_ylabel('Longitude')
+    ax.set_zlabel('Crime Stats')
+
+    plt.show()
+
+###########################################
+#FUNCTION: readData
+        #:Reads in the training Data and arguments
+###########################################
 def readData(train):
     END = int(train) - 1
     k = 1
@@ -15,6 +65,9 @@ def readData(train):
         #The columns we want to include
         included_cols = [0,1,5,6]
         #Read through all the entries and obtain the required training data
+        X = []
+        Y = []
+        Z = []
         for row in reader:
             if END >= 0:
                 content = list(row[i] for i in included_cols)
@@ -23,8 +76,11 @@ def readData(train):
                 print '[',content[2], ',',content[3] ,']'
                 END = END - 1 ;
                 k = k+1
+                X.append(lat)
+                Y.append(long)
+                Z.append(random.randint(0,5))
             else: break
-
+    plot(X,Y,Z)
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -50,7 +106,7 @@ def main(argv=None):
                 print '...DEFAULT VALUE: 100 training examples'
                 NUM = 100
                 readData(NUM)
-
+                plot()
             #raise Usage(msg)
     # more code, unchanged
     except Usage, err:
