dfcc4dacdc8c628bba4d538800f931da8f8cc318
Chaitanya Patchava <cpatchava@gmail.com>
2013-11-25 23:29:55 -0800

    fixed a bug

--- cleanup.pl
+++ cleanup.pl
@@ -1,10 +1,4 @@
-##
-#Takes alameda county data and then strips all of the excess from it
-#Spits out the data into a file called clean_data.csv
-#
-##
 #!/usr/bin/perl
-
 open (MYFILE, 'Alameda_County_Sheriff_Crime_Reports.csv');
 $count=0;
 open (OUTPUT, '>clean_data.csv');
@@ -18,7 +12,7 @@ while (<MYFILE>) {
 	$values[10] =~ s/[^0-9.]//g;
 	$values[9]= sprintf("%.3f", $values[9] );
 	$values[10]=sprintf("%.3f", $values[10]);
-	print OUTPUT "$values[9], $values[10], $values[4], $values[2], $values[6] \n";
+	print OUTPUT "$values[9], $values[10], $values[4], $values[2] \n";
  	#print "$_\n";
 	}
 	$count++;
