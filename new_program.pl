52fd74d7d43a23610350475e5072e57287d76dac
Chaitanya Patchava <cpatchava@gmail.com>
2013-11-29 12:30:03 -0800

    cleans up data before being parsed by my other program

---
+++ new_program.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+open (MYFILE, 'Braket_renew.csv');
+open (MYOUT, '>Undisturbing_data.csv');
+$count=0;
+while (<MYFILE>) {
+    chomp;
+#	print "$_";
+    my (${date},${block},${city},${state},${zip},${cc},${aid},${cid},$loc,${blank},${loc}, ${lolz}) = split("]", $_);
+	@vals = split("]", $_);
+    if($count>=0){
+		for($i=0; $i<11; $i++){
+			if($vals[6] eq "ACSO"){
+    		print MYOUT "$vals[$i]]";
+			}
+			else{
+				if($i >= 6){
+				print MYOUT "$vals[$i+1]]";
+				}
+				else{
+				print MYOUT "$vals[$i]]";
+				}
+			}
+	
+	}
+	print MYOUT "\n";
+    }
+ 
+    $count++;
+}
+
+close(MYFILE);
