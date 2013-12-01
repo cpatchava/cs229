5998934054e03feb59e65e06a5d8d9d1a202a435
Chaitanya Patchava <cpatchava@gmail.com>
2013-12-01 13:56:30 -0800

    added the capability for weight final printed

--- cleanup.pl
+++ cleanup.pl
@@ -9,12 +9,12 @@ while (<MYFILE>) {
 	chomp;
 	my @values = split(',', $_);
 	#print "$values[9], $values[10], $values[4], $values[2],$values[6]\n";
-	if($count>=0){
+	if($count>0){
 	$values[8] =~ s/[^0-9.-]//g;
 	$values[9] =~ s/[^0-9.-]//g;
 	$values[8]= sprintf("%.3f", $values[8] );
 	$values[9]=sprintf("%.3f", $values[9]);
-	print OUTPUT "$values[8], $values[9], $values[4], $values[2], $values[10], 1 \n";
+	print OUTPUT "$values[8], $values[9], $values[4], $values[2], $values[11], 1 \n";
  	#print "$_\n";
 	}
 	$count++;
@@ -26,26 +26,58 @@ open (MYFILE, 'clean_data.csv');
 $count = 0;
 $inner = 0;
 open (CROPPED, '>data_crop.csv');
-while(<MYFILE>){
+while(<MYFILE>){#take all of the info and put it into an array
 	chomp;
+	#print "$_\n";
+	#if($count <= 50){
 	@lines[$count] = "$_\n";
-	#my @lines = split(',', $_);
-	while($inner<=$count){
-		my @glob = split(',', $lines[$count]);
-		my @curr = split(',', $lines[$inner]);
-		$inner++;
-		for($i=0; $i < 7; $i++){
+	$count++;
+	#print $lines[$count-1];
+	#if(count>30){
+	#	break;
+	#}
+	#}
+}
 
-			if($glob[0] eq $curr[0]){
-			print CROPPED "$glob[$i],";	
-			}
-		
+$curr_max=1;
+$match =0;
+@clustered_set[0] = $lines[0];
+@clustered_set[1] = $lines[1];
+print "\n";
+for($i=1;$i<$count;$i++){
+	my @global_line = split(',' , $lines[$i]);#this is the current global output value
+	$j=1;
+	
+	
+	while($j<=$i){
+		@cline = split(',', $clustered_set[$j]);
+		if($global_line[0] eq $cline[0]){
+			$cline[5]++;
+			@clustered_set[$j] = "$cline[0],$cline[1],$cline[2],$cline[3],$cline[4],$cline[5]\n";
+			$match=1;
+		#	print "$clustered_set[$j]";
+			break;
 		}
+		$j++;
 	}
+
+	if($match == 0){
+		$curr_max++;
+		@clustered_set[$curr_max] = "$global_line[0],$global_line[1],$global_line[2],$global_line[3],$global_line[4],$global_line[5]";
+		#print "$clustered_set[$curr_max] \n";
+	}
+#	print "$curr_max,";
+	$match=0;	
+}
 	$inner=0;
 	print "$lines[$count]";
 
+close(MYFILE);
+$count = 0;
+while($count <= $curr_max){
+	print CROPPED "$clustered_set[$count]";
 	$count++;
 }
-close(MYFILE);
+
 close(CROPPED);
+print "The trimmed data is in data_crop.csv\n";
