dc850a3a2134e333b8a6a67f74ed70e58556425a
chaitanya <chaitanya@ubuntu.(none)>
2013-12-01 00:07:34 -0800

    made some final changes

--- new_program.pl
+++ new_program.pl
@@ -1,27 +1,27 @@
 #!/usr/bin/perl
 
-open (MYFILE, 'Braket_renew.csv');
+open (MYFILE, 'final_set.csv');
 open (MYOUT, '>Undisturbing_data.csv');
 $count=0;
 while (<MYFILE>) {
     chomp;
 #	print "$_";
-    my (${date},${block},${city},${state},${zip},${cc},${aid},${cid},$loc,${blank},${loc}, ${lolz}) = split("]", $_);
-	@vals = split("]", $_);
+#    my (${date},${block},${city},${state},${zip},${cc},${aid},${cid},$loc,${blank},${loc}, ${lolz}) = split("]", $_);
+	@vals = split("~", $_);
 		for($i=0; $i<11; $i++){
 			if($vals[6] eq "ACSO"){
-    		print MYOUT "$vals[$i]]";
+    		print MYOUT "$vals[$i] ,";
 			}
 			else{
 				if($i >= 6){
-				print MYOUT "$vals[$i+1]]";
+				print MYOUT "$vals[$i+1] ,";
 				}
 				else{
-				print MYOUT "$vals[$i]]";
+				print MYOUT "$vals[$i] ,";
 				}
 			}
 	
-	}
+		}
 	print MYOUT "\n";
  
     $count++;
